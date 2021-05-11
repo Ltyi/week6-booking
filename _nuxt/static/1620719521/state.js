c3dc23c3d474ebfcbd5f225e89ce4de043a1c049
Ryan <ltyi.box@gmail.com>
2021-05-11 15:52:11 +0800

    d1c2510

---
+++ _nuxt/static/1620719521/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fweek6-booking\u002F_nuxt\u002Fstatic\u002F1620719521",layout:"default",error:a,state:{spiner:{isOpen:false}},serverRendered:true,routePath:"\u002F",config:{app:{basePath:"\u002Fweek6-booking\u002F",assetsPath:"\u002Fweek6-booking\u002F_nuxt\u002F",cdnURL:a}}}}(null));
